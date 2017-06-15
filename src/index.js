ee07feda41855e1f8e80667b3667ef639e1937d4
lihe3 <lihe3@corp.beisencorp.com>
2017-06-15 09:32:06 +0800

    安装必要的依赖，创建文件目录

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/*import react,{Component} from 'react'
+export default class Demo extends Component{
+  render(){
+    return <div>
+      this is a demo project 
+    </div>
+  }
+}*/
+
+document.write('it works')
